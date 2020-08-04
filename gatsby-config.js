6f06c944217ecbf803829e5ca54388f8cc85464a
cwza <cwz0205a@gmail.com>
2020-08-04 09:44:48 +0800

    add google-analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // replace "UA-XXXXXXXXX-X" with your own Tracking ID
-        trackingId: 'UA-XXXXXXXXX-X'
+        trackingId: 'UA-70916999-2'
       }
     }
   ]
